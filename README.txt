a28f96041f38c3115910bde449335e76dad1f02e
Michael <waitt18@up.edu>
2018-01-21 15:52:07 -0800

    Added cmake and opencv stuff

---
+++ README.txt
@@ -0,0 +1,3 @@
+How to run:
+
+Navigate to the project directory. Then, execute "make clean", then, execute "make". An executable will be produced. Run that executable. 
--- imageCapture.cpp
+++ imageCapture.cpp
@@ -5,3 +5,12 @@
 //  Created by Michael Waitt and Kurtis Davidson on 1/24/18.
 //
 
+#include "opencv2/opencv.hpp"
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+int main() {
+	return 0;
+}
