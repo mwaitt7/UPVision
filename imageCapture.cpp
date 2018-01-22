f7255476f33d14895608a07cb95ee722a85cd433
Michael <waitt18@up.edu>
2018-01-21 18:10:41 -0800

    image capturing added, white balance fixed

--- imageCapture.cpp
+++ imageCapture.cpp
@@ -2,15 +2,24 @@
 //  imageCapture.cpp
 //
 //
-//  Created by Michael Waitt and Kurtis Davidson on 1/24/18.
+//  Created by the UPVision team on 1/24/18.
 //
 
 #include "opencv2/opencv.hpp"
 #include <iostream>
+#include <unistd.h>
 
 using namespace std;
 using namespace cv;
 
 int main() {
+	VideoCapture cap(0); //Take a pic
+
+	for(;;) {
+        Mat frame;
+        cap >> frame; // get a new frame from camera
+        imwrite("FaceFiles/yourFace.png", frame);
+        if(waitKey(30) >= 0) break;   // you can increase delay to 2 seconds here
+    }
 	return 0;
 }
