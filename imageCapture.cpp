21982f2c022d6503a3474139cbb8d06287a8e20d
almubari17 <almubari17@up.edu>
2018-02-04 14:46:26 -0800

    Add files via upload
    The video stream is working with dlib however, the .dat is missing due to GitHub not allowing anything bigger than 25MB

--- imageCapture.cpp
+++ imageCapture.cpp
@@ -1,30 +1,68 @@
+
 //
 //  imageCapture.cpp
 //
 //
 //  Created by the UPVision team on 1/24/18.
 //
+#include <utility>
+#include <dlib/opencv.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <dlib/image_processing/frontal_face_detector.h>
+#include <dlib/image_processing/render_face_detections.h>
+#include <dlib/image_processing.h>
+#include <dlib/gui_widgets.h>
 
-#include "opencv2/opencv.hpp"
-#include <iostream>
-#include <unistd.h>
-
+using namespace dlib;
 using namespace std;
-using namespace cv;
+std::vector<full_object_detection> facialFeatures;
+int main()
+{
+	try
+	{
 
-int main() {
-	VideoCapture cap(0);
-
-    //checks whether the capture is initialized or not
-    if(!cap.isOpened()) {
-        return -1;
-    }
-    
-	for(;;) {
-        Mat frame;
-        cap >> frame; //Take a pic
-        imwrite("FaceFiles/yourFace.png", frame);
-        if(waitKey(30) >= 0) break; //Loops continuously until process is terminated
-    }
-	return 0;
+		cv::VideoCapture cap(0);
+		//checks whether the capture is initialized or not
+		if (!cap.isOpened())
+		{
+			cerr << "Unable to connect to camera" << endl;
+			return 1;
+		}
+		
+		frontal_face_detector detector = get_frontal_face_detector();
+		shape_predictor pose_model;
+		image_window awindow;
+		deserialize("shape_predictor_68_face_landmarks.dat") >> pose_model;
+		while (!awindow.is_closed())
+		{
+			cv::Mat frame;
+			if (!cap.read(frame))
+			{
+				break;
+			}
+			
+			cv_image<bgr_pixel> cimg(frame);
+			
+			std::vector<rectangle> faces = detector(cimg);
+			facialFeatures.clear();
+			for (unsigned long i = 0; i < faces.size(); ++i){
+				facialFeatures.push_back(pose_model(cimg, faces[i]));
+			
+				}
+			
+			awindow.clear_overlay();
+			awindow.set_image(cimg);
+			awindow.add_overlay(render_face_detections(facialFeatures));
+		}
+	}
+	catch (serialization_error& e)
+	{
+		cout << "Cannot find .dat file check the location" << endl;
+		cout << endl << e.what() << endl;
+	}
+	catch (exception& e)
+	{
+		cout << e.what() << endl;
+	}
 }
+
