c81e7eb9e9273e4a4240732839294a81ac276943
haljaw95 <hashj95@gmail.com>
2018-02-07 23:13:10 -0800

    Added eye blinking detection.

--- imageCapture.cpp
+++ imageCapture.cpp
@@ -43,6 +43,13 @@ int main() {
 		std::clock_t start;
     	double duration;
 
+        //Used for eye blinking
+        double EYE_THRESHOLD = 0.25; //indicates a blink when EAR is below this value
+        double EYE_CONSECUTIVE_FRAMES = 2; //the # of consecutive frames the eye must be below threshold
+        double EAR = 0;
+        int counter = 0;//frame counter
+        int totalBlinks = 0;
+        
 		while (!awindow.is_closed()) {
 			//FPS Calculation
 			if (first) {
@@ -67,16 +74,78 @@ int main() {
 			}
 			
 			cv_image<bgr_pixel> cimg(frame);
-			
+            
 			std::vector<rectangle> faces = detector(cimg);
 			facialFeatures.clear();
 			for (unsigned long i = 0; i < faces.size(); ++i) {
-				facialFeatures.push_back(pose_model(cimg, faces[i]));
+                
+                facialFeatures.push_back(pose_model(cimg, faces[i]));
+                
+                double P37_41_x = facialFeatures[i].part(37).x() - facialFeatures[i].part(41).x();
+                double P37_41_y = facialFeatures[i].part(37).y() - facialFeatures[i].part(41).y();
+                
+                double P38_40_x = facialFeatures[i].part(38).x() - facialFeatures[i].part(40).x();
+                double P38_40_y = facialFeatures[i].part(38).y() - facialFeatures[i].part(40).y();
+                
+                // compute the distances between the two sets of vertical eye landmarks
+                double Dist_37_41 = sqrt((P37_41_x*P37_41_x)+ (P37_41_y*P37_41_y));
+                double Dist_38_40 = sqrt((P38_40_x*P38_40_x)+ (P38_40_y*P38_40_y));
+                
+                double P36_39_x = facialFeatures[i].part(36).x() - facialFeatures[i].part(39).x();
+                double P36_39_y = facialFeatures[i].part(36).y() - facialFeatures[i].part(39).y();
+                
+                // compute the distances between the horizontal eye landmark
+                double Dist_36_39 = sqrt((P36_39_x*P36_39_x)+ (P36_39_y*P36_39_y));
+                
+                
+                // compute the eye aspect ratio for the right eye
+                double EAR_right = (Dist_37_41 + Dist_38_40) / (2.0 * Dist_36_39);
+                
+                double P43_47_x = facialFeatures[i].part(43).x() - facialFeatures[i].part(47).x();
+                double P43_47_y = facialFeatures[i].part(43).y() - facialFeatures[i].part(47).y();
+                
+                double P44_46_x = facialFeatures[i].part(44).x() - facialFeatures[i].part(46).x();
+                double P44_46_y = facialFeatures[i].part(44).y() - facialFeatures[i].part(46).y();
+                
+                // compute the distances between the two sets of vertical eye landmarks
+                double Dist_43_47 = sqrt((P43_47_x*P43_47_x)+ (P43_47_y*P43_47_y));
+                double Dist_44_46 = sqrt((P44_46_x*P44_46_x)+ (P44_46_y*P44_46_y));
+                
+                double P42_45_x = facialFeatures[i].part(42).x() - facialFeatures[i].part(45).x();
+                double P42_45_y = facialFeatures[i].part(42).y() - facialFeatures[i].part(45).y();
+                
+                // compute the distances between the horizontal eye landmark
+                double Dist_42_45 = sqrt((P42_45_x*P42_45_x)+ (P42_45_y*P42_45_y));
+                
+                
+                // compute the eye aspect ratio for the left eye
+                double EAR_left = (Dist_43_47 + Dist_44_46) / (2.0 * Dist_42_45);
+                
+                
+                // average the EAR for both left and right eye
+                EAR =  (EAR_left + EAR_right) / 2.0;
+                
+
+                //if the avg eye aspect ratio is less than the threshold, increase frame counter
+                if (EAR < EYE_THRESHOLD) {
+                    counter += 1;
+                }
+                else {
+                    //if the frame counter is greater than or equal to the allowed number of frames
+                    //below EYE_THRESHOLD, increase the number of blinks.
+                    if (counter >= EYE_CONSECUTIVE_FRAMES) {
+                        totalBlinks += 1;
+                    }
+                    
+                    counter = 0; //reset counter
+                }
+                
 			}
 
 			float headAngle = 0;
 			//If there's a face in frame, check angle
 			if (facialFeatures.size() != 0) {
+                
 				int x1 = facialFeatures[0].part(0)(0);
 				int x2 = facialFeatures[0].part(16)(0);
 
@@ -91,7 +160,7 @@ int main() {
 			awindow.set_image(cimg);
 			awindow.add_overlay(render_face_detections(facialFeatures));
 			rectangle rect;
-			awindow.add_overlay(image_window::overlay_rect(rect, rgb_pixel(0, 0, 255), "UPVision v0.1\n\nHEAD ORIENTATION: "+ std::to_string(headAngle) + " degrees\nFRAMES PER SECOND: "+ std::to_string(fps)));
+            awindow.add_overlay(image_window::overlay_rect(rect, rgb_pixel(0, 0, 255), "UPVision v0.1\n\nHEAD ORIENTATION: "+ std::to_string(headAngle) + " degrees\nFRAMES PER SECOND: "+ std::to_string(fps)+ "\nEYE ASPECT RATIO: " + std::to_string(EAR) + "\nBLINK COUNT: " + std::to_string(totalBlinks)));
 		}
 	}
 	catch (serialization_error& e) {
@@ -102,4 +171,3 @@ int main() {
 		cout << e.what() << endl;
 	}
 }
-
